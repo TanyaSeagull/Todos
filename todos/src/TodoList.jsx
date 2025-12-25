5a9b5ed23fbc01a550da937ffa1f60571473138f
Tatiana Chaikina <introbox87@gmail.com>
2025-12-25 15:15:43 +0300

    TodoList,jsx

---
+++ todos/src/TodoList.jsx
@@ -0,0 +1,41 @@
+
+import { Box, Typography } from "@mui/material";
+
+import { useState, useEffect } from "react";
+
+const getInitialData = () => {
+  const data = JSON.parse(localStorage.getItem("todos"));
+  if (!data) return [];
+  return data;
+};
+
+export default function TodoList() {
+    const [todos, setTodos] = useState(getInitialData);
+    
+    return (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
+            m: 3,
+          }}
+        >
+          <Typography variant="h2" component="h1" sx={{ flexGrow: 1 }}>
+            Todos
+          </Typography>
+          <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+            {todos.map((todo) => (
+              <TodoItem
+                todo={todo}
+                key={todo.id}
+                remove={removeTodo}
+                toggle={() => toggleTodo(todo.id)}
+              />
+            ))}
+            <TodoForm addTodo={addTodo} />
+          </List>
+        </Box>
+      );
+}
